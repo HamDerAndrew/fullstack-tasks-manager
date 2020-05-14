054518c3e3d66501d8340870b22cd06564377576
André Larsen <andrewsondergaard@gmail.com>
2020-05-14 15:22:49 +0200

    Feature: 'login' endpoint with basic auth

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(value) {
@@ -40,6 +41,17 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+// Create a method in the User model with 'userSchema.statics.nameOfMethod'
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email: email})
+    if (!user) throw new Error('Unable to log in')
+
+    const isAMatch = await bcrypt.compare(password, user.password)
+    if (!isAMatch) throw new Error('Unable to log in')
+
+    return user
+} 
 
 // Setup middleware. 
 // Using a normal function because the 'this' binding is crucial here. Arrow functions don't bind 'this'
