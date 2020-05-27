0f37f8f3442a2305ee33080ce27e77f288d6c28b
André Larsen <andrewsondergaard@gmail.com>
2020-05-27 23:25:53 +0200

    Feature: adding CORS policy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require('cors')
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT
+
+app.use(cors());
 
 // Set Express to automatically parse JSON so we can access it in request handlers
 app.use(express.json())
